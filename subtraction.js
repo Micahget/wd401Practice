60d8000c31568dc7d864ad989ec8e91c604d4be9
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2024-02-20 14:32:37 +0530

    One test case added

--- subtraction.js
+++ subtraction.js
@@ -1,5 +1,5 @@
 function sub(a,b){
 return a-b;
 }
-module.exports = sub;
 
+module.exports = sub; // export the function
--- subtraction.test.js
+++ subtraction.test.js
@@ -1,4 +1,15 @@
+// import both funtions from subtraction.js
 const subtraction = require("./subtraction");
+const add = require("./add");
+
+// subtraction for 2 nos
+
 test("subtracts 4 - 2 to equal 2", () => {
   expect(subtraction(4, 2)).toBe(2);
 });
+
+// addition for 2 nos
+test("adds 1 + 2 to equal 3", () => {
+  expect(add(1, 2)).toBe(3);
+}
+);
