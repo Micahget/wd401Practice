ff10350b1be5f6fe1b55659b41a38ff290529732
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2024-02-20 15:15:51 +0530

    feat: add Multiplication
    multiplication test for multiply function is added. This is to learn more about ci/cd

--- opetations.test.js
+++ opetations.test.js
@@ -1,6 +1,7 @@
 // import both funtions from subtraction.js
 const subtraction = require("./subtraction");
 const add = require("./add");
+const multiply = require("./multiply");
 
 // subtraction for 2 nos
 
@@ -13,3 +14,7 @@ test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
 }
 );
+
+test("multiply 2 * 2 to equal 4", () => {
+  expect(multiply(2, 2)).toBe(4);
+});
