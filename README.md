0978fa77d2af4073172952096ca746dcfb51c4dd
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2024-02-20 13:32:19 +0530

    feat: Add workflow and Jest files
    Testing our pipeline with jest by adding some simple test

---
+++ README.md
@@ -0,0 +1,3 @@
+## Basics of Git Flow
+
+1.I am updating initializing of git flow WD401 Practice starting from level 4
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./css/styles.css";
+import './lazymodule.js';
+
+
+
+document.getElementById('lazy-button').addEventListener('click', () => {
+import('./lazyModule. js') . then ((module) => {
+    module.default();
+}
+);
+}
+);
+    
+
+console. log('Hello world');
