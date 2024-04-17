4bb1a376a07c1acd1992c1c990d2fea91a66551c
Mikiyas Getachew <104720395+Micahget@users.noreply.github.com>
2024-04-17 23:33:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 /* eslint-disable */
 const app = require('./app')
 app.listen(3000, () => {
-    console.log('Server listening on port 3000')
-    })  
+   try {
+     console.log('Server listening on port 3000')
+   } catch (error) {
+        console.log('Error starting server: ', error)
+   }
+})  
