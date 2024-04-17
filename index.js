ffee5a81f80b4e41eb99a650f457a0d3e467c565
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2024-04-17 23:34:38 +0530

    add some comment to demonestrate merge conflict

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const app = require('./app')
 app.listen(3000, () => {
-    console.log('Server listening on port 3000')
-    })  
+  console.log("Server listening on port 3000");
+  // add some comments here to demonestrate stuff.
+})  
